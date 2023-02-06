7bb26215ee298f55f4d608fefe58417456db4103
skaur93 <svirk309@gmail.com>
2023-02-06 15:57:35 -0500

    Bug fixes

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -8,10 +8,11 @@ function Alert(props) {
   }
   return (
    
-      props.alert && <div className="alert alert-warning alert-dismissible fade show" role="alert">
+    <div style={{height:'50px'}}>
+     { props.alert && <div className="alert alert-warning alert-dismissible fade show" role="alert">
         {capitalize(props.alert.type)} : 
         {props.alert.message}
-       
+       </div>}
     </div>
    
   )
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -25,12 +25,12 @@ export default function Textform(props) {
           <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', 
             color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
           </div>
-          <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
-          <button className="btn btn-primary mx-2" onClick={handlelowClick}>Convert to lowercase</button>
+          <button className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to uppercase</button>
+          <button className="btn btn-primary mx-2 my-1" onClick={handlelowClick}>Convert to lowercase</button>
       </div>
       <div className='container my-5' style={{color: props.mode==='dark'?'white':'black'}}>   
           <h1>Text summary</h1>
-          <p> {text.split(" ").filter((element)=>{ return element.length !== 0 }).length} words and {text.length} characters</p>
+          <p> {text.split(/\s+/).filter((element)=>{ return element.length !== 0 }).length} words and {text.length} characters</p>
           <p>{0.008 * text.split(" ").length} Minutes read</p>
           <h2>Preview</h2>
           <p>{text.length>0?text:'Enter something to preview'}</p>
